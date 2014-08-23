d5341d12f328f93b2c688e8c2d54aa9a18b32af4
msalli <alli.cernoch@gmail.com>
2014-08-23 13:19:16 -0700

    adding Users controller, all User views (index, new, show)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+  #view all users
+  def index
+    @users = User.all
+  end
+
+  #form to create a new user
+  def new
+    @user = User.new
+  end
+
+
+  def create
+    new_user = params.require(:user).permit(:first_name, :last_name, :email, :image_url)
+    User.create(new_user)
+
+    p new_user
+    redirect_to "/users"
+  end
+
+  #show individual user
+  def show
+    user_id = params[:id]
+    @user = User.find_by_id(user_id)
+  end
+
+
 end
