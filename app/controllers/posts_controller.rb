da7525594d476aa2190386467bb5c2351837432a
smonette <steph@stephmonette.com>
2014-08-23 14:38:38 -0700

    getting the new post form working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,18 +6,14 @@ class PostsController < ApplicationController
 
   def create
     @user = User.find_by_id(params[:user_id])
-
-      new_post = params.require(:post).permit(:title, :body)
-      @post = @user.posts.create(new_post)
-
-      render :show
+    new_post = params.require(:post).permit(:title, :body)
+    @post = @user.posts.create(new_post)
+    render :show
   end
 
   def show
     id = params[:id]
     @post = Post.find_by_id(id)
-
     render :show
-
   end
 end
