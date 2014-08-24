8e46276fd558a29d60d8a859be8cd46c573b30cc
cameronjacoby <cameron.jacoby@gmail.com>
2014-08-24 01:32:23 -0700

    implement tag capabilities for new post & edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,16 +10,21 @@ class PostsController < ApplicationController
   end
 
   def new
-      @user = User.find_by_id(params[:user_id])
-      @post = @user.posts.new
+    @user = User.find_by_id(params[:user_id])
+    @post = @user.posts.new
   end
 
   def create
-
     @user = User.find_by_id(params[:user_id])
     new_post = params.require(:post).permit(:title, :body)
     @post = @user.posts.new(new_post)
+    tag_params = params[:tags].split(",").map(&:strip).map(&:downcase)
+
     if @post.save
+        tag_params.each do |tag_str|
+        tag = Tag.find_or_create_by(name: tag_str)
+        @post.tags << tag
+      end
       redirect_to [@user, @post]
     else
       render action: 'new'
@@ -34,11 +39,20 @@ class PostsController < ApplicationController
   def update
     @user = User.find_by_id(params[:user_id])
     @post = @user.posts.find_by_id(params[:id])
-    update_post = params.require(:post).permit(:title, :body)
 
-    @post.update_attributes(
-      :title => update_post[:title],
-      :body => update_post[:body])
+    if @post
+      update_post = params.require(:post).permit(:title, :body)
+      @post.update_attributes(
+        :title => update_post[:title],
+        :body => update_post[:body])
+
+      @post.tags.clear
+      tag_params = params.require(:post).permit(:tags)[:tags].split(",").map(&:strip).map(&:downcase)
+      tag_params.each do |tag_str|
+        tag = Tag.find_or_create_by(name: tag_str)
+        @post.tags << tag
+      end
+    end
     render :show
   end
 
