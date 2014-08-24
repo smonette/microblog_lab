402e864696c7823bbaa62931f342f7a7dd853234
smonette <steph@stephmonette.com>
2014-08-24 13:03:12 -0700

    fixing the pages controller with alli and cameron

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,69 +1,54 @@
 class PagesController < ApplicationController
 
-	def index
-		res = params[:value]
-		p res
+  def index
+    res = params[:value]
+    p res
 
-	end
+  end
 
-	def show
-		page_id = params[:id]
-		u_id = params[:user_id]
-
-		@user = User.find_by_id(u_id)
-		@page = Page.find_by_id(page_id)
-
-
-	end
-
-	def new
-		user_id = params[:user_id]
-		@user = User.find(user_id)
-
-		@page = @user.pages.new
-	end
-
-	#working but cannot keep the user from adding more than one
-	#about and contact
-	def create
-		user_id = params[:user_id]
-		new_content = params.require(:page).permit(:content, :name)
-		# page = Page.users.find_by_id(params[:id])
-		# p page
-		user = User.find(user_id)
-
-		my_page = Page.create(new_content)
-
-		user.pages << my_page
-
-		redirect_to "/users/#{user.id}"
-	end
- #edit page working
-	def edit
-    user_id = params[:id]
-    @user = User.find_by_id(user_id)
-
+  def show
     page_id = params[:id]
+    u_id = params[:user_id]
+    @user = User.find_by_id(u_id)
+
     @page = Page.find_by_id(page_id)
 
+    # @pages = @user.pages
+    # p @pages
+  end
 
-	end
-#working on update
-	def update
+  def new
+    user_id = params[:user_id]
+    @user = User.find(user_id)
 
-		user_id = params[:id]
-		user = User.find(user_id)
+    @page = @user.pages.new
+  end
 
-		page_id = params[:id]
-    page = Page.find_by_id(page_id)
-    res = params.require(:page).permit(:name, :content)
 
-    Page.update_attributes(
-      :name => res[:name],
-      :context  => res[:context])
+  def create
+    user_id = params[:user_id]
+    @user = User.find(user_id)
 
-    redirect_to "/users/#{user_id}/pages/#{page_id}"
-	end
+    new_page = params.require(:page).permit(:name, :content)
+
+    if @user.pages.length == 1
+        @user.pages.each do |page|
+            if page[:name] == new_page[:name]
+              redirect_to "/users/#{user_id}"
+            else
+              my_page = Page.create(new_page)
+              @user.pages << my_page
+              redirect_to "/users/#{user_id}"
+            end
+        end
+    elsif @user.pages.length == 0
+      my_page = Page.create(new_page)
+      @user.pages << my_page
+      redirect_to "/users/#{user_id}"
+    else
+      redirect_to "/users/#{user_id}"
+    end
+  end
 
 
 end
