e648959dad3118bf35d7e68e613d4a4804441845
cameronjacoby <cameron.jacoby@gmail.com>
2014-08-23 17:40:27 -0700

    edit entry & tag specs to pass validations

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Tag, :type => :model do
 
   it "should test that tag is saved" do
-   Tag.create(:name => "BobTag")
-   bobstag = Tag.find_by_name("BobTag")
+   Tag.create(:name => "bobtag")
+   bobstag = Tag.find_by_name("bobtag")
    expect(bobstag).not_to eq(nil)
   end
 
