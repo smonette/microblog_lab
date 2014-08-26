91a0dfb36832bba46c9c7122d4b321bd9e42ce5c
smonette <steph@stephmonette.com>
2014-08-26 16:16:28 -0700

    adding sickurity to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
   def new
     @user = current_user
     @post = @user.posts.new
-    if @user != @current_user
+    if User.find(params[:user_id]) != @current_user
       redirect_to root_path
     end
   end
@@ -45,39 +45,51 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @user = User.find_by_id(params[:user_id])
+    @user = current_user
     @post = @user.posts.find_by_id(params[:id])
-    if @user != @current_user
+    if User.find(params[:user_id]) != @current_user
       redirect_to root_path
     end
   end
 
   def update
-    @user = User.find_by_id(params[:user_id])
-    @post = @user.posts.find_by_id(params[:id])
+    if User.find(params[:user_id]) == current_user
+      @user = current_user
+      @post = @user.posts.find_by_id(params[:id])
 
-    if @post
-      update_post = params.require(:post).permit(:title, :body)
-      @post.update_attributes(
-        :title => update_post[:title],
-        :body => update_post[:body])
+      if @post
+        update_post = params.require(:post).permit(:title, :body)
+        @post.update_attributes(
+          :title => update_post[:title],
+          :body => update_post[:body])
 
-      @post.tags.clear
-      tag_params = params.require(:post).permit(:tags)[:tags].split(",").map(&:strip).map(&:downcase)
-      tag_params.each do |tag_str|
-        tag = Tag.find_or_create_by(name: tag_str)
-        @post.tags << tag
+        @post.tags.clear
+        tag_params = params.require(:post).permit(:tags)[:tags].split(",").map(&:strip).map(&:downcase)
+        tag_params.each do |tag_str|
+          tag = Tag.find_or_create_by(name: tag_str)
+          @post.tags << tag
+        end
       end
+      flash[:post_edited] = "Message"
+      render :show
+    else
+      flash[:notice] = "oops please re-login..."
+      session[:user_id] = nil
+      redirect_to "/login"
     end
-    flash[:post_edited] = "Message"
-    render :show
   end
 
   def destroy
-    @user = User.find_by_id(params[:user_id])
-    @post = @user.posts.find(params[:id])
-    @post.destroy
-    redirect_to "/users/#{@user.id}"
+    if User.find(params[:user_id]) == current_user
+      @user = current_user
+      @post = @user.posts.find(params[:id])
+      @post.destroy
+      redirect_to "/users/#{@user.id}"
+     else
+      flash[:notice] = "oops please re-login..."
+      session[:user_id] = nil
+      redirect_to "/login"
+    end
   end
 
 end
